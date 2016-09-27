7364e3304fc0f1ed383bc26cb5ba9d6b1528a4a3
Evan <evan@simplymeasured.com>
2016-09-27 14:07:02 -0700

    create ui for inventory and slots

---
+++ core/src/com/evansitzes/game/inventory/SlotTooltip.java
@@ -0,0 +1,7 @@
+package com.evansitzes.game.inventory;
+
+/**
+ * Created by evan on 9/27/16.
+ */
+public class SlotTooltip {
+}
---
+++ core/src/com/evansitzes/game/inventory/TooltipListener.java
@@ -0,0 +1,11 @@
+package com.evansitzes.game.inventory;
+
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+
+/**
+ * Created by evan on 9/27/16.
+ */
+public class TooltipListener implements EventListener {
+    public TooltipListener(SlotTooltip tooltip, boolean b) {
+    }
+}
